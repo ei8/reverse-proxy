2949c316f32556f9eb62c57f2afe233d54966570
elmerbool <35117496+elmerbool@users.noreply.github.com>
2021-12-30 20:50:52 +1100

    Enabled config of Staging certificate usage.

--- src/main/Program.cs
+++ src/main/Program.cs
@@ -4,11 +4,12 @@ using Microsoft.Extensions.DependencyInjection;
 using ReverseProxy;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddLettuceEncrypt();
-builder.Services.AddSingleton<ICertificateSource, FilePemSource>();
+if (bool.TryParse(builder.Configuration["LettuceEncrypt:UseStagingServer"], out bool useStagingServer) && useStagingServer)
+    builder.Services.AddSingleton<ICertificateSource, FilePemSource>();
 
-builder.Services.AddReverseProxy()
+builder.Services
+    .AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
